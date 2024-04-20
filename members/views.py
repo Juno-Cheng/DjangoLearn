d1209dcfa5dcd4b914e87cdef2a0284f2d3fbea3
Juno-Cheng <chengjonathan2021@gmail.com>
2024-04-20 01:15:29 -0700

    Added URL to App

--- members/views.py
+++ members/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def world(request):
+    return HttpResponse("Hello world!")
+
+
+# Views pass information to browser - Returns an HTTP Response that has the details
+# Also takes in HTML with render & so on
+
+# URL.py calls these function depending how it is defined
